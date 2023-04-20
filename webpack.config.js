633660cf3731f73d4b29c72f74eaa932d5d7379a
shubh30 <shubhamnagpal30@gmail.com>
2023-04-20 12:21:33 +0530

    Add sass loader support

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,9 @@ module.exports = {
     }, {
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.s[ac]ss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
     }]
   },
   devServer: {
